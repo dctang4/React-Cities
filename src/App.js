84d3871f626817af0936311679292e6f1a70b5ed
Chun Hin D Tang <80924016+dctang4@users.noreply.github.com>
2021-04-22 10:29:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './styles.css';
+import imageData from './imageData'
+// console.log(imageData)
+
+export default function App() {
+  const [bigImg, setBigImg] = useState(imageData[0].url);
+
+  const images = imageData.map((image, index) => {
+    // const [currentImg, setCurrentImg] = useState(images[0].props.src);
+    const handleClick = () => setBigImg(image.url)
+    // const clickBorder = () => {
+    //   return ( )
+    // }
+    return (
+      <img 
+        onClick={handleClick}
+        className="thumb" 
+        style={{border: bigImg === image.url ? '4px solid green' : ''}}
+        src={image.url}
+        alt={image.place}
+        key={index}
+      />
+    )
+  });
+
+    // const handleCurrentImg = (index) => setCurrentImg(images[index].props.src)
+
+  // console.log(images[0].props.src)
+
+  return (
+    <div className="App">
+      <h1>Cities Of The World</h1>
+      <div id="wrapper">
+        <div id="thumbnails">
+          {images}
+        </div>
+        <img  id="bigimage" src={bigImg} alt="bigimage" />
+      </div>
+    </div>
+  );
+}
---
+++ src/imageData.js
@@ -0,0 +1,34 @@
+export default [
+{
+  "url": "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "london",
+},
+{
+  "url":"https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "amsterdam",
+},
+{
+  "url": "https://images.unsplash.com/photo-1524820197278-540916411e20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "sidney",
+},
+{
+  "url": "https://images.unsplash.com/photo-1547448526-5e9d57fa28f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "tokyo",
+},
+{
+  "url": "https://images.unsplash.com/photo-1592409065737-a253f2900fa8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "dallas",
+},
+{
+  "url": "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  "place": "paris",
+},
+{
+  "url": "https://images.unsplash.com/photo-1505884065216-0661d68e5c47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "athens",
+},
+{
+  "url": "https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  "place": "dubai",
+},
+]
